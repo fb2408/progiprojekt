ba73704758b4e8e42740ff0147ad5335dabde23a
Marko Barbir <marko.barbir@fer.hr>
2021-10-13 00:24:05 +0200

    Initialized Spring Boot project

---
+++ source/src/main/java/hr/fer/proinz/proggers/parkshare/ParkShareApplication.java
@@ -0,0 +1,13 @@
+package hr.fer.proinz.proggers.parkshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkShareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParkShareApplication.class, args);
+    }
+
+}
---
+++ source/src/test/java/hr/fer/proinz/proggers/parkshare/ParkShareApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.fer.proinz.proggers.parkshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkShareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
