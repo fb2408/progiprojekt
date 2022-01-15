3318874a07cb74ec87e25fee90d6723fc24a48b1
Marko Barbir <marko.barbir@fer.hr>
2022-01-15 23:48:38 +0100

    =add title for parking entrance

---
+++ source/src/main/java/hr/fer/proinz/proggers/parkshare/controller/BaseController.java
@@ -0,0 +1,19 @@
+//package hr.fer.proinz.proggers.parkshare.controller;
+//
+//import hr.fer.proinz.proggers.parkshare.dto.RegisterFormDTO;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.ui.ModelMap;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.servlet.ModelAndView;
+//
+//@Controller
+//@RequestMapping("/")
+//public class BaseController {
+//    @GetMapping
+//    public String getIndex(Model model){
+//        model.addAttribute("registerForm", new RegisterFormDTO());
+//        return "index";
+//    }
+//}
